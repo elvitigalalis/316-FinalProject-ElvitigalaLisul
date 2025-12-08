b0272652db0a9a303f8b5ae2c0aa10c7f44b4d6f
elvitigalalis <elvitigalalis@gmail.com>
2025-12-08 01:59:20 -0500

    Modify home screen

--- client/src/components/HomeScreen.js
+++ client/src/components/HomeScreen.js
@@ -1,72 +1,64 @@
-import { useContext, useEffect } from 'react'
-import { GlobalStoreContext } from '../store'
-import PlaylistCard from './PlaylistCard.js'
-import MUIDeleteModal from './MUIDeleteModal'
+import { useContext, useEffect } from "react";
+import { GlobalStoreContext } from "../store";
+import PlaylistCard from "./PlaylistCard.js";
+import MUIDeleteModal from "./MUIDeleteModal";
 
-import AddIcon from '@mui/icons-material/Add';
-import Fab from '@mui/material/Fab'
-import List from '@mui/material/List';
-import Box from '@mui/material/Box'
+import AddIcon from "@mui/icons-material/Add";
+import Fab from "@mui/material/Fab";
+import List from "@mui/material/List";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
 /*
     This React component lists all the top5 lists in the UI.
     
     @author McKilla Gorilla
-    @author elvitigalalis
 */
 const HomeScreen = () => {
-    const { store } = useContext(GlobalStoreContext);
+  const { store } = useContext(GlobalStoreContext);
 
-    useEffect(() => {
-        store.loadIdNamePairs();
-    }, []);
+  useEffect(() => {
+    store.loadIdNamePairs();
+  }, []);
 
-    function handleCreateNewList() {
-        store.createNewList();
-    }
-    let listCard = "";
-    if (store) {
-        listCard = 
-            <List sx={{width: '100%', bgcolor: 'background.paper', mb:"20px" }}>
-            {
-                store.idNamePairs.map((pair) => (
-                    <PlaylistCard
-                        key={pair._id}
-                        idNamePair={pair}
-                        selected={false}
-                    />
-                ))
-                
-            }
-            <Fab sx={{transform:"translate(1150%, 10%)"}}
-                color="primary" 
-                aria-label="add"
-                id="add-list-button"
-                onClick={handleCreateNewList}
-            >
-                <AddIcon />
-            </Fab>
-            </List>;
-    }
-    return (
-        <div id="playlist-selector">
-            <div id="list-selector-heading">
-            <Fab sx={{transform:"translate(-20%, 0%)"}}
-                color="primary" 
-                aria-label="add"
-                id="add-list-button"
-                onClick={handleCreateNewList}
-            >
-                <AddIcon />
-            </Fab>
-                Your Playlists
-            </div>
-            <Box sx={{bgcolor:"background.paper"}} id="list-selector-list">
-                {
-                    listCard
-                }
-                <MUIDeleteModal />
-            </Box>
-        </div>)
-}
+  function handleCreateNewList() {
+    store.createNewList();
+  }
+
+  return (
+    <Box id="playlist-selector">
+      <Box id="playlist-selector-heading">
+        <Typography variant="h3" id="playlist-selector-heading-text">
+          Your Playlists
+        </Typography>
+
+        <Box sx={{ position: "absolute", right: 20 }}>
+          <Fab
+            color="primary"
+            aria-label="add"
+            id="add-list-button"
+            onClick={handleCreateNewList}
+          >
+            <AddIcon />
+          </Fab>
+        </Box>
+      </Box>
+
+      <Box id="playlist-selector-list">
+        <List
+          sx={{
+            width: "90%",
+            bgcolor: "background.paper",
+            borderRadius: 2,
+            mb: 2,
+          }}
+        >
+          {store.idNamePairs.map((pair) => (
+            <PlaylistCard key={pair._id} idNamePair={pair} selected={false} />
+          ))}
+        </List>
+      </Box>
+    </Box>
+  );
+};
 
 export default HomeScreen;
