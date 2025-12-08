20adcae3c1d211bec0db607ee60d238c28142d08
elvitigalalis <elvitigalalis@gmail.com>
2025-12-08 01:14:57 -0500

    Delete unused imports

--- client/src/components/LoginScreen.js
+++ client/src/components/LoginScreen.js
@@ -3,14 +3,12 @@ import AuthContext from "../auth";
 import MUIErrorModal from "./MUIErrorModal";
 import Copyright from "./Copyright";
 
-import Avatar from "@mui/material/Avatar";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
 import Grid from "@mui/material/Grid";
 import Link from "@mui/material/Link";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
-import Paper from "@mui/material/Paper";
 import TextField from "@mui/material/TextField";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
